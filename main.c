a930ba1efb0abb07b96d35408cb1bfd3f2e1f2c9
FrederikvSvane <s224766@student.dtu.dk>
2023-09-06 18:37:59 +0200

    main-3: Erosion virker

--- main.c
+++ main.c
@@ -2,74 +2,155 @@
 #include <stdio.h>
 #include "cbmp.h"
 
-//Function to invert pixels of an image (negative)
-void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
+// Function to invert pixels of an image (negative)
+void invert(unsigned char input_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS])
+{
   for (int x = 0; x < BMP_WIDTH; x++)
   {
-    for (int y = 0; y < BMP_HEIGTH; y++)
+    for (int y = 0; y < BMP_HEIGHT; y++)
     {
       for (int c = 0; c < BMP_CHANNELS; c++)
       {
-      output_image[x][y][c] = 255 - input_image[x][y][c];
+        output_image[x][y][c] = 255 - input_image[x][y][c];
       }
     }
   }
 }
 
-void grey_scale(unsigned char rgb_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char grey_scale_image[BMP_WIDTH][BMP_HEIGTH]){
-  for (int x=0; x < BMP_WIDTH; x++){
-    for (int y=0; y < BMP_HEIGTH; y++){
+void grey_scale(unsigned char rgb_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS], unsigned char grey_scale_image[BMP_WIDTH][BMP_HEIGHT])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGHT; y++)
+    {
       unsigned char red = rgb_image[x][y][0];
       unsigned char green = rgb_image[x][y][1];
       unsigned char blue = rgb_image[x][y][2];
 
-      unsigned char grey = (red+green+blue)/3;
+      unsigned char grey = (red + green + blue) / 3;
 
       grey_scale_image[x][y] = grey;
     }
   }
 }
 
-void convert_2d_to_3d(unsigned char grey_scale_image[BMP_WIDTH][BMP_HEIGTH], unsigned char rgb_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]){
-  for(int x=0;x < BMP_WIDTH;x++){
-    for(int y=0; y < BMP_HEIGTH; y++){
-      rgb_image[x][y][0]=grey_scale_image[x][y];
-      rgb_image[x][y][1]=grey_scale_image[x][y];
-      rgb_image[x][y][2]=grey_scale_image[x][y];
+void binary_threshold(unsigned char grey_scale_image[BMP_WIDTH][BMP_HEIGHT], unsigned char black_white_image[BMP_WIDTH][BMP_HEIGHT])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGHT; y++)
+    {
+      unsigned char threshold = 90;
+      if (grey_scale_image[x][y] <= threshold)
+      {
+        black_white_image[x][y] = 0;
+      }
+      else
+      {
+        black_white_image[x][y] = 255;
+      }
     }
   }
 }
 
-  //Declaring the array to store the image (unsigned char = unsigned 8 bit)
-  unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
-  unsigned char grey_image[BMP_WIDTH][BMP_HEIGTH];
+void erode(unsigned char black_white_image[BMP_WIDTH][BMP_HEIGHT], unsigned char eroded_image[BMP_WIDTH][BMP_HEIGHT])
+{
+  unsigned char erosion_structure[3][3] = {{0, 1, 0}, {1, 1, 1}, {0, 1, 0}};
+  unsigned char temp_image[BMP_WIDTH][BMP_HEIGHT];
 
-//Main function
-int main(int argc, char** argv){
-  //argc counts how may arguments are passed
-  //argv[0] is a string with the name of the program
-  //argv[1] is the first command line argument (input image)
-  //argv[2] is the second command line argument (output image)
-
-  //Checking that 2 arguments are passed
-  if (argc != 3)
+  for (int x = 0; x < BMP_WIDTH; x++)
   {
-      fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
-      exit(1);
+    for (int y = 0; y < BMP_HEIGHT; y++)
+    {
+      temp_image[x][y] = black_white_image[x][y];
+    }
   }
 
+  for (int x = 1; x < BMP_WIDTH - 1; x++)
+  {
+    for (int y = 1; y < BMP_HEIGHT - 1; y++)
+    {
+      int shouldErode = 0;
+      for (int dx = -1; dx <= 1; dx++)
+      {
+        for (int dy = -1; dy <= 1; dy++)
+        {
+          if (erosion_structure[dx + 1][dy + 1] == 1 && black_white_image[x + dx][y + dy] == 0)
+          {
+            shouldErode = 1;
+            break;
+          }
+        }
+        if (shouldErode)
+          break;
+      }
+      if (shouldErode)
+        temp_image[x][y] = 0;
+    }
+  }
+
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGHT; y++)
+    {
+      eroded_image[x][y] = temp_image[x][y];
+    }
+  }
+}
+
+void convert_2d_to_3d(unsigned char grey_scale_image[BMP_WIDTH][BMP_HEIGHT], unsigned char rgb_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS])
+{
+  for (int x = 0; x < BMP_WIDTH; x++)
+  {
+    for (int y = 0; y < BMP_HEIGHT; y++)
+    {
+      rgb_image[x][y][0] = grey_scale_image[x][y];
+      rgb_image[x][y][1] = grey_scale_image[x][y];
+      rgb_image[x][y][2] = grey_scale_image[x][y];
+    }
+  }
+}
+
+// Declaring the array to store the image (unsigned char = unsigned 8 bit)
+unsigned char input_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS];
+unsigned char output_image[BMP_WIDTH][BMP_HEIGHT][BMP_CHANNELS];
+unsigned char grey_image[BMP_WIDTH][BMP_HEIGHT];
+unsigned char black_white_image[BMP_WIDTH][BMP_HEIGHT];
+unsigned char eroded_image[BMP_WIDTH][BMP_HEIGHT];
+
+// Main function
+int main(int argc, char **argv)
+{
+  // argc counts how may arguments are passed
+  // argv[0] is a string with the name of the program
+  // argv[1] is the first command line argument (input image)
+  // argv[2] is the second command line argument (output image)
+
+  // Checking that 2 arguments are passed
+  if (argc != 3)
+  {
+    fprintf(stderr, "Usage: %s <output file path> <output file path>\n", argv[0]);
+    exit(1);
+  }
 
   printf("Example program - 02132 - A1\n");
 
-  //Load image from file
+  // Load image from file
   read_bitmap(argv[1], input_image);
 
   grey_scale(input_image, grey_image);
 
-  convert_2d_to_3d(grey_image, output_image);
+  binary_threshold(grey_image, black_white_image);
 
-  //Save image to file
+  erode(black_white_image, eroded_image);
+  
+  for(int i=0; i < 5; i++){
+    erode(eroded_image, eroded_image);
+  }
+
+  convert_2d_to_3d(eroded_image, output_image);
+
+  // Save image to file
   write_bitmap(output_image, argv[2]);
 
   printf("Done!\n");
