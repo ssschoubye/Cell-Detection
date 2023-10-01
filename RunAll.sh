7ad84f93ba6028ac072fedf078609e5732014cec
FrederikvSvane <s224766@student.dtu.dk>
2023-10-01 22:26:38 +0200

    main: Alle output billeder med otsus vedlagt

--- RunAll.sh
+++ RunAll.sh
@@ -1,6 +1,6 @@
 mkdir -p output_images
-for i in {1..5}; do
-  input_filename="samples/impossible/${i}IMPOSSIBLE.bmp"
+for i in {1..10}; do
+  input_filename="samples/hard/${i}HARD.bmp"
   output_filename="output_images/output_image${i}.bmp"
   ./main.exe "$input_filename" "$output_filename"
 done
